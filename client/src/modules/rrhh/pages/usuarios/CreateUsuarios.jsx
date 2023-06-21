3c2fe3d1de8e6c5ac62441efa6c71bd5862a7bb1
Andrew <ajacoboc@unsa.edu.pe>
2023-06-21 00:29:46 -0500

    Estructura del Frontend y librerias empleadas para el proyecto

---
+++ client/src/modules/rrhh/pages/usuarios/CreateUsuarios.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export const CreateUsuarios = () => {
+  return (
+    <div>CreateUsuarios</div>
+  )
+}
---
+++ client/src/modules/rrhh/pages/usuarios/ListUsuarios.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export const ListUsuarios = () => {
+  return (
+    <div>ListUsuarios</div>
+  )
+}

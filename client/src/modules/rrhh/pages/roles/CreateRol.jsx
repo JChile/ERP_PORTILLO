4308b41b13c273f7706adfe060f305facd20bd40
Andrew <ajacoboc@unsa.edu.pe>
2023-07-11 20:26:08 -0500

    Estrcuturacion de Gestion de usuarios

--- client/src/modules/rrhh/pages/roles/CreateRol.jsx
+++ client/src/modules/rrhh/pages/roles/CreateRol.jsx
@@ -1,32 +1,32 @@
-import React from 'react'
-import { Button } from '@mui/material'
-import { Link, useNavigate } from 'react-router-dom'
+import React from "react";
+import { Button } from "@mui/material";
+import { Link, useNavigate } from "react-router-dom";
 
 const CreateRol = () => {
-
-  const navigate = useNavigate()
-  const [form, setForm] = React.useState({rolName: ""})
+  const navigate = useNavigate();
+  const [form, setForm] = React.useState({ rolName: "" });
+  const { rolName } = form;
 
   const handleChange = (event) => {
-    const {name, value } = event.target
-    setForm(prev => ({
+    const { name, value } = event.target;
+    setForm((prev) => ({
       ...prev,
-      [name]: value
-    }))
-  }
+      [name]: value,
+    }));
+  };
 
   const handleSubmit = (event) => {
-    event.preventDefault()
-    console.log(form)
-  }
+    event.preventDefault();
+    console.log(form);
+  };
 
   return (
-    <div className='w-4/5  max-w-screen-sm mx-auto flex flex-col gap-y-5'>
-      <h1 className='text-center font-semibold text-2xl'>Registrar Rol</h1>
+    <div className="w-4/5  max-w-screen-sm mx-auto flex flex-col gap-y-5">
+      <h1 className="text-center font-semibold text-2xl">Registrar Rol</h1>
       <form
         onSubmit={handleSubmit}
-        method='post'
-        className='min-w-[242px] flex flex-col gap-y-6'
+        method="post"
+        className="min-w-[242px] flex flex-col gap-y-6"
       >
         <label className="block flex flex-col gap-y-1">
           <span className="after:content-['*'] after:ml-0.5 after:text-yellow-500 block text-sm font-medium">
@@ -37,17 +37,17 @@ const CreateRol = () => {
             name="rolName"
             className="mt-1 px-3 py-2 bg-white border shadow-sm border-slate-300 placeholder-slate-400 focus:outline-none focus:border-sky-500 focus:ring-sky-500 block w-full rounded-md sm:text-sm focus:ring-1"
             placeholder="Administrador"
-            id='rolName'
-            value={form.rolName}
+            id="rolName"
+            value={rolName}
             onChange={handleChange}
           />
         </label>
 
-        <div className='flex justify-end gap-x-5'>
+        <div className="flex justify-end gap-x-5">
           <Link to={""}>
             <Button
-              variant='contained'
-              color='error'
+              variant="contained"
+              color="error"
               size="small"
               onClick={() => navigate(-1)}
             >
@@ -55,17 +55,17 @@ const CreateRol = () => {
             </Button>
           </Link>
           <Button
-            variant='contained'
-            color='success'
-            type='submit'
+            variant="contained"
+            color="success"
+            type="submit"
             size="small"
           >
             Guardar
           </Button>
         </div>
-      </form >
-    </div >
-  )
-}
+      </form>
+    </div>
+  );
+};
 
-export default CreateRol
+export default CreateRol;
--- client/src/modules/rrhh/pages/usuarios/ListUsuarios.jsx
+++ client/src/modules/rrhh/pages/usuarios/ListUsuarios.jsx
@@ -1,7 +1,10 @@
-import React from 'react'
+import React from "react";
+import { Link } from "react-router-dom";
 
 export const ListUsuarios = () => {
   return (
-    <div>ListUsuarios</div>
-  )
-}
+    <>
+      <h1>LIST USUARIOS</h1>
+    </>
+  );
+};

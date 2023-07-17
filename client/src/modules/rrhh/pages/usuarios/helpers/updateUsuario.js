09acf53d95127eb8b7688569cd2642a5740a5a9a
Andrew <ajacoboc@unsa.edu.pe>
2023-07-17 04:26:53 -0500

    Modificacion de solicitud put de gestion de usuarios

--- client/src/modules/rrhh/pages/usuarios/helpers/updateUsuario.js
+++ client/src/modules/rrhh/pages/usuarios/helpers/updateUsuario.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 export const updateUsuario = async (idUsuario, body) => {
   const DOMAIN = import.meta.env.VITE_BACKEND_URL;
-  const URL = `${DOMAIN}/api/user_profile/`;
+  const URL = `${DOMAIN}/api/user_profile/${idUsuario}`;
 
   const { data } = await axios.put(URL, {
     ...body,

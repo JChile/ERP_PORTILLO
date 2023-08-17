09baf1fe985fb0ce8f90a24c255f11aa714dc2d2
bgomezv123 <bgomezv@unsa.edu.pe>
2023-08-17 15:57:33 -0500

    list view y retrive view personalizado para campañas vfinal

--- marketing/views.py
+++ marketing/views.py
@@ -5,6 +5,8 @@ from rest_framework import generics
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
 from rest_framework.permissions import IsAuthenticated
+from django.shortcuts import get_object_or_404
+
 
 
 
@@ -39,25 +41,23 @@ class CampaniaList(generics.ListCreateAPIView):
     queryset = Campania.objects.all()
 
     def list(self, request):
-        queryset = Campania.objects.all()
-        groupserializer = CampaniaSerializer(queryset, many=True)
-        permissions = Proyecto.objects.all()
+        campania_queryset = Campania.objects.all()
+        groupserializer = CampaniaSerializer(campania_queryset, many=True)
+        proyecto_queryset = Proyecto.objects.all()
         users = User.objects.all()
         subCategorias = SubCategoria.objects.all()
         categorias = Categoria.objects.all()
-
-
         dataJson = groupserializer.data
         for i in dataJson:
-            permissions_data = permissions.filter(id=i["proyecto"])
-            user_data = users.filter(id=i["user"])
-            subcategoria_data = subCategorias.filter(id=i["subCategoria"])
-            categoria_data = categorias.filter(id=subcategoria_data[0].categoria.id)
+            permissions_data = proyecto_queryset.get(id=i["proyecto"])
+            user_data = users.get(id=i["user"])
+            subcategoria_data = subCategorias.get(id=i["subCategoria"])
+            categoria_data = categorias.get(id=subcategoria_data.categoria.id)
             
-            permissionSerializer = ProyectoSerializer(permissions_data,many = True)
-            userSerializer = UserSerializer(user_data,many = True)
-            subCategoriaSerializer = SubCategoriaSerializer(subcategoria_data,many = True)
-            categoriaSerializer = CategoriaSerializer(categoria_data,many = True)
+            permissionSerializer = ProyectoSerializer(permissions_data)
+            userSerializer = UserSerializer(user_data)
+            subCategoriaSerializer = SubCategoriaSerializer(subcategoria_data)
+            categoriaSerializer = CategoriaSerializer(categoria_data)
 
             i["proyecto"] = permissionSerializer.data
             i["user"] = userSerializer.data
@@ -70,3 +70,27 @@ class CampaniaList(generics.ListCreateAPIView):
 class CampaniaDetail(generics.RetrieveUpdateDestroyAPIView):
     serializer_class = CampaniaSerializer
     queryset = Campania.objects.all()
+
+    def retrieve(self, request, pk=None):
+        campania_queryset = Campania.objects.all()
+        campania = get_object_or_404(campania_queryset, pk=pk)
+        campaniaSerializer = CampaniaSerializer(campania)
+        dataJson = campaniaSerializer.data
+
+        proyecto = Proyecto.objects.all().get(id=dataJson["proyecto"])
+        user = User.objects.all().get(id=dataJson["user"])
+        subCategoria = SubCategoria.objects.all().get(id=dataJson["subCategoria"])
+        categoria = Categoria.objects.all().get(id=subCategoria.categoria.id)
+
+
+        proyectoSerializer = ProyectoSerializer(proyecto)
+        userSerializer = UserSerializer(user)
+        subCategoriaSerializer = SubCategoriaSerializer(subCategoria)
+        categoriaSerializer = CategoriaSerializer(categoria)
+
+        dataJson["proyecto"] = proyectoSerializer.data
+        dataJson["user"] = userSerializer.data
+        dataJson["subCategoria"] = subCategoriaSerializer.data
+        dataJson["categoria"] = categoriaSerializer.data
+
+        return Response(dataJson)

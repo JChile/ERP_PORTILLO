b4bca4d766ba74f06f8276a4ae9ed70c4761a10d
JChile <64179028+JChile@users.noreply.github.com>
2023-08-29 18:09:05 -0500

    Actualizar modelo lead

--- ventas/serializers.py
+++ ventas/serializers.py
@@ -20,3 +20,18 @@ class LlamadaSerializer(serializers.ModelSerializer):
     class Meta:
         model = Llamada
         fields = '__all__'
+
+class EstadoSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Estado
+        fields = '__all__'
+
+class ObjecionSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Objecion
+        fields = '__all__'
+
+class AsesorProyectoSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = AsesorProyecto
+        fields = '__all__'

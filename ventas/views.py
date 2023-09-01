4995c5850f2765e84a7a1f44614f93133b8f3424
JChile <64179028+JChile@users.noreply.github.com>
2023-08-31 23:25:32 -0500

    actualizar campos de usuario de asesor

--- ventas/views.py
+++ ventas/views.py
@@ -1,6 +1,12 @@
+from cuenta.serializers import *
 from django.shortcuts import render
 from ventas.serializers import *
 from rest_framework import generics
+from rest_framework.response import Response
+from django.shortcuts import get_object_or_404
+
+
+
 
 class LeadList(generics.ListCreateAPIView):
     serializer_class = LeadSerializer
@@ -14,9 +20,39 @@ class AsesorList(generics.ListCreateAPIView):
     serializer_class = AsesorSerializer
     queryset = Asesor.objects.all()
 
+    def list(self, request):
+        asesor_queryset = Asesor.objects.all()
+        users = User.objects.all()
+        groupserializer = AsesorSerializer(asesor_queryset, many=True)
+        dataJson = groupserializer.data
+        user_fields = ["id", "username", "first_name", "last_name"]
+        for i in dataJson:
+            user_data = users.get(id=i["user"])
+            userSerializer = UserSerializer(user_data)
+            user_data_serialized = userSerializer.data
+            i["user"] = {field: user_data_serialized[field] for field in user_fields}
+        
+        return Response(dataJson)
+
+
+
 class AsesorDetail(generics.RetrieveUpdateDestroyAPIView):
     serializer_class = AsesorSerializer
     queryset = Asesor.objects.all()
+
+    def retrieve(self, request, pk=None):
+        asesor_queryset = Asesor.objects.all()
+        asesor = get_object_or_404(asesor_queryset, pk=pk)
+        asesorserializer = AsesorSerializer(asesor)
+        dataJson = asesorserializer.data
+        user_fields = ["id", "username", "first_name", "last_name"]
+        
+        user = User.objects.all().get(id=dataJson["user"])
+        userSerializer = UserSerializer(user)
+        user_data_serialized = userSerializer.data
+        dataJson["user"] = {field: user_data_serialized[field] for field in user_fields}
+
+        return Response(dataJson)
 
 class WhatsAppList(generics.ListCreateAPIView):
     serializer_class = WhatsAppSerializer
@@ -33,6 +69,24 @@ class LlamadaList(generics.ListCreateAPIView):
 class LlamadaDetail(generics.RetrieveUpdateDestroyAPIView):
     serializer_class = LlamadaSerializer
     queryset = Llamada.objects.all()
+
+class EstadoList(generics.ListCreateAPIView):
+    serializer_class = EstadoSerializer
+    queryset = Estado.objects.all()
+
+class EstadoDetail(generics.RetrieveUpdateDestroyAPIView):
+    serializer_class = EstadoSerializer
+    queryset = Estado.objects.all()
+
+class ObjecionList(generics.ListCreateAPIView):
+    serializer_class = ObjecionSerializer
+    queryset = Objecion.objects.all()
+
+class ObjecionDetail(generics.RetrieveUpdateDestroyAPIView):
+    serializer_class = ObjecionSerializer
+    queryset = Objecion.objects.all()
+
+
 
 
 

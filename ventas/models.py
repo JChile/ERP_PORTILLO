f854a307b5a877b28ed9d43e169124d9deef7e19
JChile <64179028+JChile@users.noreply.github.com>
2023-09-26 21:01:51 -0500

    Update models.py

--- ventas/models.py
+++ ventas/models.py
@@ -38,6 +38,7 @@ class Lead(models.Model):
     campania = models.ForeignKey(Campania, on_delete=models.CASCADE, null=True,blank=True)
     objecion = models.ForeignKey(Objecion, on_delete=models.SET_NULL, null=True, blank=True)
     estado = models.ForeignKey(EstadoRegistro,on_delete=models.SET_NULL, default='A',null=True)
+    estadoLead = models.ForeignKey(EstadoLead,on_delete=models.SET_NULL, default='NP', null=True, blank=True)
     
     def __str__(self):
         return self.nombre
@@ -54,7 +55,11 @@ class Llamada(models.Model):
     estado = models.ForeignKey(EstadoRegistro,on_delete=models.SET_NULL, default='A',null=True)
 
 
-   
+class EstadoLead(models.Model):
+    estado = models.CharField(max_length=2, primary_key=True)
+    nombre = models.CharField(max_length=50, null=True, default=None)
+    def __str__(self):
+        return self.estado
 
 
    

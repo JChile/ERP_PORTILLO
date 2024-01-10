bbdfa5c95fe09a538418f67996d872e2b3e61564
rony <ronyramos495@gmail.com>
2024-01-10 13:35:14 -0500

    cambios realizados

--- client/src/modules/proyectos/helpers/getProyectos.js
+++ client/src/modules/proyectos/helpers/getProyectos.js
@@ -2,16 +2,13 @@ import axios from "axios";
 
 const DOMAIN = import.meta.env.VITE_BACKEND_URL;
 
-export const getProyectos= async ({authToken}) => {
+export const getProyectos = async ({ authToken }) => {
   const URL = `${DOMAIN}/api/proyecto/`;
-  const { data } = await axios.get(
-    URL,
-    {
-      headers: {
-        "Content-Type": "application/json",
-        Authorization: `Bearer ${authToken}`,
-      },
-    }
-    );
+  const { data } = await axios.get(URL, {
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${authToken}`,
+    },
+  });
   return data;
 };

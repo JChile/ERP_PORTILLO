bbc08088c8b428b98922281cd678532da2c0df55
rony <ronyramos495@gmail.com>
2024-01-17 03:26:11 -0500

    cambios en la configuración de las vistas de lead para jefe de ventas

--- client/src/modules/lead/controllers/HomeController.jsx
+++ client/src/modules/lead/controllers/HomeController.jsx
@@ -1,20 +1,18 @@
 import React, { useContext } from "react";
 import { AuthContext } from "../../../auth";
-import ListLeadController from "./ListLeadController";
-import JefeVentasDashboard from "../pages/jefeFinanciero/JefeVentasDashboard";
-import AsesorLeadDashboard from "../pages/asesor/AsesorLeadDashboard";
 import { NoAccessLeads } from "../components";
+import ListJefeVentasLead from "../components/JefeFinanciero/ListJefeVentasLead";
 
 const HomeController = () => {
   const { currentUser, authTokens } = useContext(AuthContext);
 
   switch (currentUser.groups) {
     case "asesor": {
-      console.log("ASESOR");
+      //console.log({admin: currentUser.isAdmin});
       return <AsesorLeadDashboard token={authTokens["access"]} />;
     }
     case "jefe_ventas": {
-      return <JefeVentasDashboard token={authTokens["access"]} />;
+      return <ListJefeVentasLead credentials={authTokens["access"]} />;
     }
     default:
       return <NoAccessLeads />;

3322dd933fe4776609ba30fa52ff21f331b3fdbd
rony <ronyramos495@gmail.com>
2024-01-17 06:27:04 -0500

    cambios en el raa

--- client/src/modules/lead/controllers/HomeController.jsx
+++ client/src/modules/lead/controllers/HomeController.jsx
@@ -1,11 +1,8 @@
 import React, { useContext } from "react";
 import { AuthContext } from "../../../auth";
 import { NoAccessLeads } from "../components";
-<<<<<<< HEAD
 import MarketingDashboard from "../pages/marketing/MarketingLeadDashboard";
-=======
 import ListJefeVentasLead from "../components/JefeFinanciero/ListJefeVentasLead";
->>>>>>> 495b386b4d3134d9bf2ee2afe6f6c6bd493788b5
 
 const HomeController = () => {
   const { currentUser, authTokens } = useContext(AuthContext);

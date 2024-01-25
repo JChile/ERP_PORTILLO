9424b9e58538c8f945c07fd5c8f5c76bdcc43f66
bgomezv123 <bgomezv@unsa.edu.pe>
2024-01-24 23:24:59 -0500

    se modifico la vista de leads con la query de asginado o desagsinados

--- ventas/views.py
+++ ventas/views.py
@@ -38,13 +38,11 @@ class LeadList(generics.ListCreateAPIView):
         desde = request.query_params.get('desde')
         hasta = request.query_params.get('hasta')
         asignado = request.query_params.get('asignado')
-        
-        print("ESTADOOOOOOOOOOOOO", estado)
-        
+             
         if asignado:
-            lead_queryset = self.queryset.filter(asignado=asignado)
+            lead_queryset = lead_queryset.filter(asignado=asignado)
         if estado:
-            lead_queryset = self.queryset.filter(estado=estado)
+            lead_queryset = lead_queryset.filter(estado=estado)
         if desde and hasta:
             lead_queryset = lead_queryset.filter(horaRecepcion__range=[desde, hasta])
 
@@ -352,21 +350,17 @@ class EventoList(generics.ListCreateAPIView):
         if not (bool(request.user.groups.first().permissions.filter(codename=PermissionEvento.CAN_VIEW) or request.user.is_superuser)):
             return Response({"message": "Usuario no tiene permisos para ver eventos"}, status=403)
 
+        evento_queryset = Evento.objects.all()
         estado = request.query_params.get('estado')
         desde = request.query_params.get('desde')
         hasta = request.query_params.get('hasta')
-        print(estado)
-        
-        
+ 
         
         if estado:
             evento_queryset = Evento.objects.all().filter(estado=estado)
-        else:
-            evento_queryset = Evento.objects.all()
-        
-        
         if desde and hasta:
             evento_queryset = evento_queryset.filter(fecha_creacion__range=[desde, hasta])
+
 
         if request.user.isAdmin == False:
             evento_queryset = evento_queryset.filter(asesor=usuarioId)

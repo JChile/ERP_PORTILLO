4f7f903eae16299a29d3f817c99268d92fe41292
bgomezv123 <bgomezv@unsa.edu.pe>
2024-01-25 19:44:44 -0500

    se creo la app multimedia para productos y proyectos

---
+++ multimedia/admin.py
@@ -0,0 +1,12 @@
+from django.contrib import admin
+from .models import *
+# Register your models here.
+
+
+admin.site.register(VideoProducto)
+admin.site.register(VideoProyecto)
+
+admin.site.register(ImagenProyecto)
+
+admin.site.register(ImagenProducto)
+

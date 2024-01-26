951fc554fb7a29903f3e8538ebac176f3ccb7e5d
bgomezv123 <bgomezv@unsa.edu.pe>
2024-01-26 13:23:10 -0500

    se creo un trabajo que se ejecuta cada 1 minutos

--- cuenta/serializers.py
+++ cuenta/serializers.py
@@ -1,6 +1,4 @@
 from django.contrib.auth.models import Group, Permission
-from . import views
-
 from rest_framework import serializers
 from .models import *
 from django.contrib.auth.hashers import make_password

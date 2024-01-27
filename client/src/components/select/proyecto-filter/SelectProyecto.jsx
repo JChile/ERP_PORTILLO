8b760c3ddd8ce6e8c33405cbd0be551122e710f8
John Dow <ajacoboc@unsa.edu.pe>
2024-01-27 18:31:43 -0500

    Leads no asignados al 90%

---
+++ client/src/components/select/proyecto-filter/SelectProyecto.jsx
@@ -0,0 +1,52 @@
+import React, { useState, useContext, useEffect } from "react";
+import { getProyectos } from "./getProyectos";
+import { FormControl, MenuItem, Select } from "@mui/material";
+import { AuthContext } from "../../../auth";
+
+export const SelectProyecto = ({ onNewInput, size = "small" }) => {
+  const { authTokens } = useContext(AuthContext);
+  const [options, setOptions] = useState([]);
+  const [value, setValue] = useState("");
+
+  const obtenerProyectos = async () => {
+    const result = await getProyectos({ authToken: authTokens["access"] });
+    const formatSelect = result.map((element) => {
+      return {
+        value: element.id,
+        label: element.nombre,
+      };
+    });
+    setOptions(formatSelect);
+  };
+
+  const handleChange = (event) => {
+    const selectValue = event.target.value;
+    const name = "proyecto";
+    onNewInput(selectValue, name);
+    setValue(selectValue + "");
+  };
+
+  useEffect(() => {
+    obtenerProyectos();
+  }, []);
+
+  return (
+    <FormControl sx={{ m: 1, minWidth: 120 }} size={size}>
+      <Select
+        value={value}
+        onChange={handleChange}
+        displayEmpty
+        inputProps={{ "aria-label": "Without label" }}
+      >
+        <MenuItem value="">
+          <em>None</em>
+        </MenuItem>
+        {options.map((element, index) => (
+          <MenuItem key={element.value} value={element.label}>
+            {element.label}
+          </MenuItem>
+        ))}
+      </Select>
+    </FormControl>
+  );
+};

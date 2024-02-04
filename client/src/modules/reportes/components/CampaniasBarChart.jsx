a5a2d1e06993ec673f4d3ce4aa729a27b936fa21
begazo-ticona-jesus <jbegazoti@unsa.edu.pe>
2024-02-04 08:25:56 -0500

    reporte retorno por lead

---
+++ client/src/modules/reportes/components/CampaniasBarChart.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import { LineChart, Line, CartesianGrid, XAxis, YAxis, Tooltip, BarChart, Bar } from 'recharts';
+
+const data = [
+  { name: 'Año Nuevo', coste_estimado: 700, coste_real: 800 },
+  { name: 'ProyectoX_organico', coste_estimado: 0, coste_real: 0 },
+];
+
+export const CampaniasBarChart = () => (
+  <BarChart width={500} height={300} data={data}>
+    <CartesianGrid strokeDasharray="3 3" />
+    <XAxis dataKey="name" />
+    <YAxis />
+    <Tooltip />
+    <Bar type="monotone" dataKey="coste_estimado" stroke="#8884d8" activeDot={{ r: 8 }} />
+    <Bar type="monotone" dataKey="coste_real" stroke="#82ca9d" />
+  </BarChart>
+);

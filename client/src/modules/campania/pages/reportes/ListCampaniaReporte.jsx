d27a0723c827db931056f52a0ec5e0a81f71c993
romzc <ronyramos495@gmail.com>
2024-03-07 18:12:47 -0500

    se agrego la vista para mostrar los reportes

--- client/src/modules/campania/pages/reportes/ListCampaniaReporte.jsx
+++ client/src/modules/campania/pages/reportes/ListCampaniaReporte.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import {
   Table,
   TableBody,
@@ -7,88 +7,87 @@ import {
   TableHead,
   TableRow,
   Paper,
+  TextField,
+  Button,
+  Typography,
 } from "@mui/material";
-
-const data = {
-  campaign_info: [
-    {
-      mkt: "WHATSAPP",
-      weeks: [
-        { leads: 12, investment: 12.2 },
-        { leads: 12, investment: 12.2 },
-        { leads: 12, investment: 12.2 },
-        { leads: 12, investment: 12.2 },
-        { leads: 12, investment: 12.2 },
-      ],
-    },
-    {
-      mkt: "LLAMADA",
-      weeks: [
-        { leads: 1, investment: 0.0 },
-        { leads: 1, investment: 0.0 },
-        { leads: 1, investment: 0.0 },
-        { leads: 1, investment: 0.0 },
-        { leads: 1, investment: 0.0 },
-      ],
-    },
-  ],
-};
+import { AuthContext } from "../../../../auth";
+import { dataMapper } from "../../utils/CampaniaMapper";
+import { DatePicker } from "@mui/x-date-pickers";
+import dayjs from "dayjs";
+import { FilterProyectos } from "../../../../components";
 
 export default function ListCampaniaReportes() {
+  const { authTokens } = useContext(AuthContext);
+  const [data, setData] = useState(null);
+  const [proyecto, setProyecto] = useState();
+  const [selectedDate, setSelectedDate] = useState(dayjs(""));
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const formatQuery = `proyecto=${proyecto}&anio=${selectedDate.year()}&mes=${
+      selectedDate.month() + 1
+    }`;
+    const info = await dataMapper({
+      query: formatQuery,
+      token: authTokens["access"],
+    });
+    setData(info);
+  };
+
+  const onAddProyecto = (item) => {
+    setProyecto(item.id);
+  };
+
   return (
     <div>
-      <h1 className="text-2xl uppercase">{`Cuadro de inversión campañas - ${"Alamos"}`}</h1>
-      <CampaignTable campaignInfo={data.campaign_info} />
+      <Typography variant="h4" gutterBottom>
+        Cuadro de inversión de campañas
+      </Typography>
+      <form onSubmit={handleSubmit} className="flex flex-col gap-2">
+        <div className="w-[25ch] ml-2">
+          <FilterProyectos
+            onNewInput={onAddProyecto}
+            defaultValue={proyecto}
+            size="medium"
+          />
+        </div>
+        <DatePicker
+          views={["month", "year"]}
+          defaultValue={new Date()}
+          label="Seleccione una fecha"
+          value={selectedDate}
+          onChange={(newValue) => setSelectedDate(newValue)}
+          sx={{ m: 1, width: "25ch" }}
+        />
+        <Button variant="contained" type="submit" sx={{ m: 1, width: "25ch" }}>
+          Buscar
+        </Button>
+      </form>
+      {data && <CampaignTable headers={data.header} rows={data.rows} />}
+      {data && (
+        <CampaignTable
+          headers={data.costoLeadHeader}
+          rows={data.costoLeadRows}
+        />
+      )}
+      {data && (
+        <CampaignTable
+          headers={data.leadAsignadosHeader}
+          rows={data.leadAsignadosRows}
+        />
+      )}
+      {data && (
+        <CampaignTable
+          headers={data.costoLeadAsesorHeader}
+          rows={data.costoLeadAsesorRows}
+        />
+      )}
     </div>
   );
 }
 
-const CampaignTable = ({ campaignInfo }) => {
-  // Función para obtener las cabeceras de la tabla
-  const getTableHeaders = () => {
-    let campaignLength = 0;
-    const headers = ["Campaña"];
-    const [firstCampaign = null] = campaignInfo;
-    if (firstCampaign != null) {
-      campaignLength = firstCampaign.weeks.length;
-    }
-    for (let i = 0; i < campaignLength; i++) {
-      headers.push(`Semana ${i + 1}`);
-      headers.push(`Inversión ${i + 1}`);
-    }
-
-    return headers;
-  };
-
-  // Función para obtener las filas de la tabla
-  const getTableRows = () => {
-    const rows = [];
-    const weeklyReport = [];
-    let indexI = 0;
-    let indexJ = 1;
-
-    campaignInfo.forEach((campaign) => {
-      const rowData = [campaign.mkt];
-      campaign.weeks.forEach((week) => {
-        rowData.push(week.leads);
-        rowData.push(week.investment);
-        if (!weeklyReport[indexI]) weeklyReport[indexI] = 0;
-        if (!weeklyReport[indexJ]) weeklyReport[indexJ] = 0;
-        weeklyReport[indexI] += week.leads;
-        weeklyReport[indexJ] += week.investment;
-        indexI += 2;
-        indexJ += 2;
-      });
-      indexI = 0;
-      indexJ = 1;
-      rows.push(rowData);
-    });
-
-    const totalRow = ["MKT TOTAL", ...weeklyReport];
-    rows.push(totalRow);
-    return rows;
-  };
-
+const CampaignTable = ({ headers, rows }) => {
   return (
     <TableContainer component={Paper} className="mt-4">
       <Table>
@@ -101,7 +100,7 @@ const CampaignTable = ({ campaignInfo }) => {
               },
             }}
           >
-            {getTableHeaders().map((header, index) => (
+            {headers.map((header, index) => (
               <TableCell key={index} align="center" className="font-medium">
                 {header}
               </TableCell>
@@ -109,7 +108,7 @@ const CampaignTable = ({ campaignInfo }) => {
           </TableRow>
         </TableHead>
         <TableBody>
-          {getTableRows().map((row, rowIndex) => (
+          {rows.map((row, rowIndex) => (
             <TableRow key={rowIndex}>
               {row.map((cell, cellIndex) => (
                 <TableCell key={cellIndex} align="center">

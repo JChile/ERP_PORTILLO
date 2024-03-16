59263016f8469a09caf1eafbb1f9e4df96851d15
bgomezv123 <bgomezv@unsa.edu.pe>
2024-03-16 00:36:42 -0500

    se añadio un max_length a celular2

--- portillo_erp/settings.py
+++ portillo_erp/settings.py
@@ -205,7 +205,8 @@ MEDIA_ROOT = BASE_DIR / 'media/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 CORS_ALLOWED_ORIGINS = [
-    'http://localhost:5173'
+    'http://localhost:5173',
+    ''
 ]
 # CRONJOBS = [
 #     ('*/1 * * * *', 'ventas.cron.my_scheduled_job',

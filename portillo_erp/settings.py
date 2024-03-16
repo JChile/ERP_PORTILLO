7662d0c75c1d81396feb13e6a58872663d2f7dfa
bgomezv123 <bgomezv@unsa.edu.pe>
2024-03-16 00:53:17 -0500

    se agrego token de expiracion 30 dias

--- portillo_erp/settings.py
+++ portillo_erp/settings.py
@@ -118,8 +118,8 @@ DATABASES = {
 
 
 SIMPLE_JWT = {
-    "ACCESS_TOKEN_LIFETIME": timedelta(hours=4),
-    "REFRESH_TOKEN_LIFETIME": timedelta(hours=4),
+    "ACCESS_TOKEN_LIFETIME": timedelta(days=30),
+    "REFRESH_TOKEN_LIFETIME": timedelta(days=365),
     "ROTATE_REFRESH_TOKENS": True,
     "BLACKLIST_AFTER_ROTATION": True,
     "UPDATE_LAST_LOGIN": False,
